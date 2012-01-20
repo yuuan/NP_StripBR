cfdd81724467aa9cb322f1e7b452ede5af935fa3
yuuAn <berkana@yuuan.net>
2012-01-20 23:48:58 +0900

    my first commit

--- NP_StripBR.php
+++ NP_StripBR.php
@@ -1,23 +1,23 @@
-<?php 
- 
+<?php
+
 // plugin needs to work on Nucleus versions <=2.0 as well
 if (!function_exists('sql_table')) {
 	function sql_table($name) {
 		return 'nucleus_' . $name;
 	}
 }
- 
-class NP_StripBR extends NucleusPlugin { 
-    function getName() {    return 'StripBR'; } 
-    function getAuthor() {  return 'IWAMA Kazuhiko'; } 
-    function getURL() {     return 'http://www.sera.desuyo.net/'; } 
-    function getVersion() { return '0.01'; } 
-    function getDescription() { 
-        return 'Remove linebreaks'; 
-    } 
-    function getEventList() { 
-        return array('PreItem'); 
-    } 
+
+class NP_StripBR extends NucleusPlugin {
+    function getName() {    return 'StripBR'; }
+    function getAuthor() {  return 'IWAMA Kazuhiko / yuuAn'; }
+    function getURL() {     return 'http://www.sera.desuyo.net/'; }
+    function getVersion() { return '0.01.1'; }
+    function getDescription() {
+        return 'Remove linebreaks';
+    }
+    function getEventList() {
+        return array('PreItem');
+    }
 	function supportsFeature($what) {
 		switch($what) {
 			case 'SqlTablePrefix':
@@ -26,24 +26,34 @@ class NP_StripBR extends NucleusPlugin {
 				return 0;
 		}
 	}
- 
-    function replaceCallback($matches) { 
-        return removeBreaks($matches[1]); 
-    } 
- 
-    function event_PreItem($data) { 
-        $this->currentItem = &$data["item"]; 
-        $this->currentItem->body = preg_replace_callback( 
-                '#<%nobr%>(.*?)<%/nobr%>#s', 
-                array(&$this, 'replaceCallback'), 
-                $this->currentItem->body 
-            ); 
-        $this->currentItem->more = preg_replace_callback( 
-                '#<%nobr%>(.*?)<%/nobr%>#s', 
-                array(&$this, 'replaceCallback'), 
-                $this->currentItem->more 
-            ); 
-    } 
- 
-} 
+
+    function replaceCallback($matches) {
+        return removeBreaks($matches[1]);
+    }
+
+    function event_PreItem($data) {
+        $this->currentItem = &$data["item"];
+        $this->currentItem->body = preg_replace_callback(
+                '#<%nobr%>(.*?)<%/nobr%>#s',
+                array(&$this, 'replaceCallback'),
+                $this->currentItem->body
+            );
+        $this->currentItem->more = preg_replace_callback(
+                '#<%nobr%>(.*?)<%/nobr%>#s',
+                array(&$this, 'replaceCallback'),
+                $this->currentItem->more
+            );
+        $this->currentItem->body = preg_replace(
+        	    '#<%nobr/%>(:?<br />)?#s',
+        	    '',
+                $this->currentItem->body
+            );
+        $this->currentItem->more = preg_replace(
+        	    '#<%nobr/%>(:?<br />)?#s',
+        	    '',
+                $this->currentItem->more
+            );
+    }
+
+}
 ?>
